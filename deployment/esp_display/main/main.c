69c5cbc2f5dbb3bc94d3b227cac9129088102bb3
fourthstanza <kitkatmatthew@hotmail.com>
2026-05-15 21:43:07 -0400

    added sleep & touch initialization

--- deployment/esp_display/main/main.c
+++ deployment/esp_display/main/main.c
@@ -3,15 +3,20 @@
 #include "board.h"
 #include "display.h"
 #include "lvgl_display.h"
+#include "touch_panel.h"
+#include "esp_log.h"
+#include "esp_sleep.h"
 
 void app_main(void)
 {
     printf("Booted, beginning program...\n");
 
     esp_lcd_panel_handle_t panel_handle = display_startup();
-    lvgl_display_init(panel_handle);
+    esp_lcd_touch_handle_t touch_handle = touch_panel_startup();
+    lvgl_display_init(panel_handle, touch_handle);
+    esp_sleep_enable_ext0_wakeup(PIN_NUM_TOUCH_INT, 0);
     
     while(1) {
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
